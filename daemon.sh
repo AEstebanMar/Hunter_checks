3fbe90bfc07a093976d31cbb9bee3dc2a0abd8e0
Álvaro Esteban Martos <alvaroesteban@uma.es>
2025-01-08 13:42:03 +0100

    AutoFlow variables now depend on Data directory

--- daemon.sh
+++ daemon.sh
@@ -3,15 +3,13 @@
 
 source ~soft_bio_267/initializes/init_autoflow
 export CODE_PATH=`pwd`
+data_dir=$CODE_PATH/Data
 source $CODE_PATH/config_daemon
 template=$CODE_PATH/templates/template.af
 script_dir=$CODE_PATH/scripts
-input=$CODE_PATH/reduced_counts.txt
 target=$CODE_PATH/my_target.txt
-library_sizes=/mnt/scratch/users/bio_267_uma/elenarojano/NGS_projects/HepG2_PMM2/results/mapping_reports/metric_table
 rnaseq_input_path=$CODE_PATH/ctrl_vs_dox_cell
 mirnaseq_input_path=$CODE_PATH/CTL_vs_DOX_cell
-Multimir_path=~josecordoba/proyectos/multimir_db/p_hsa
 add_opt_corr='--save_temp:--tag_filter:prevalent;prevalent:-u:2:--output_pairs:validated'
 strats=\"\"
 mode=$1
@@ -19,15 +17,16 @@ aux_opt=$2
 
 variables=`echo -e "
 	\\$root_dir=$CODE_PATH,
+	\\$data_dir=$data_dir,
 	\\$script_dir=$script_dir,
-	\\$input=$input,	
-	\\$target=$target,
+	\\$input=$data_dir/reduced_counts.txt,	
+	\\$target=$data_dir/my_target.txt,
 	\\$output_dir=$output_dir,
-	\\$library_sizes=$library_sizes,
-	\\$rnaseq_input_path=$rnaseq_input_path,
+	\\$library_sizes=$data_dir/metric_table,
+	\\$rnaseq_input_path=$data_dir/ctrl_vs_dox_cell,
 	\\$strats=$strats,
-	\\$mirnaseq_input_path=$mirnaseq_input_path,
-	\\$Multimir_path=$Multimir_path,
+	\\$mirnaseq_input_path=$data_dir/CTL_vs_DOX_cell,
+	\\$Multimir_path=$data_dir/p_hsa,
 	\\$CORR_THRS='-0.55:-0.6:-0.65:-0.7:-0.75:-0.8:-0.85:-0.9:-0.95',
 	\\$add_opt_corr=$add_opt_corr,
 	\\$cormit_data_path=$CODE_PATH/cormit_data,
