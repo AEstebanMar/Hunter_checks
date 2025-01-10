68ec600d1cc6c56c5acae95c7ec4f5e2b91431ab
Álvaro Esteban Martos <alvaroesteban@uma.es>
2025-01-10 16:41:13 +0100

    Removed unused variable, no longer passed to AutoFlow

--- daemon.sh
+++ daemon.sh
@@ -29,7 +29,6 @@ variables=`echo -e "
 	\\$Multimir_path=$data_dir/p_hsa,
 	\\$CORR_THRS='-0.55:-0.6:-0.65:-0.7:-0.75:-0.8:-0.85:-0.9:-0.95',
 	\\$add_opt_corr=$add_opt_corr,
-	\\$cormit_data_path=$CODE_PATH/cormit_data,
 	\\$organism='hsa',
 	\\$aux_parsers_path=$CODE_PATH/aux_parsers,
 	\\$fun_pvalue=0.5,
